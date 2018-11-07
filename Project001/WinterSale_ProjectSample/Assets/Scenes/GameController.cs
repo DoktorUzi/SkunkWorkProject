a55b0bdceaed3fd1421fa7d2b9968f2ed65286c5
DoktorUzi <40932111+DoktorUzi@users.noreply.github.com>
2018-11-07 08:17:11 +0100

    Added WorkInProgress Sample Project for WinterSale Game

---
+++ Project001/WinterSale_ProjectSample/Assets/Scenes/GameController.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+   /********************/
+   /* Public Variables */
+   /********************/
+   
+   /* Prefab elements for List of games */
+   public float mainSceneDurationSeconds= 15.0F; // Duration of the main scene in seconds
+	public GameObject Prototype1;   // Input prefab gameobject 1
+	public GameObject Prototype2;   // Input prefab gameobject 2
+	public GameObject Prototype3;   // Input prefab gameobject 3
+	public GameObject Prototype4;   // Input prefab gameobject 4
+	public GameObject Prototype5;   // Input prefab gameobject 5
+	public GameObject Prototype6;   // Input prefab gameobject 6
+	public GameObject Prototype7;   // Input prefab gameobject 7
+	public GameObject Prototype8;   // Input prefab gameobject 8
+	public GameObject Prototype9;   // Input prefab gameobject 9
+	public GameObject Prototype10;  // Input prefab gameobject 10
+   
+   /* Cameras of the scene */
+   public Camera mainSceneCamera;
+   public Camera menuCamera;
+   public Camera gameOverCamera;
+   
+   public const int TOT_OBJECT_NUM      = 10; // Number of available elements for the list
+   public const int LIST_ELEMENTS_NUM   = 10; // Number of elements in the list
+   
+   /* Variable containing the list of gathered objects IDs. Global, updated from ObjectBehavior */
+   public static List<int> objList = new List<int>();
+   
+   /********************/
+   /* Local variables **/
+   /********************/
+   
+   private List<GameObject> GamePrefab = new List<GameObject>();
+   private float initialTime; // Variable to store initial time of the principal scene
+   private int i; // Loop counter
+   
+   private enum en_MachineState {IDLE, OPENING, MAINMENU, INTRO, SCENE, OUTRO, ENDGAME};
+   private en_MachineState MachineState = en_MachineState.IDLE; // Initialized state to 'IDLE'
+
+   
+   /********************/
+   /* Public functions */
+   /********************/
+   
+	// Use this for initialization
+	void Start () {
+      /* Set initial state of the Cameras */
+      
+      mainSceneCamera = (GameObject.FindWithTag("MainCamera")).GetComponent(typeof(Camera)) as Camera;
+      menuCamera = (GameObject.FindWithTag("MenuCamera")).GetComponent(typeof(Camera)) as Camera;
+      gameOverCamera = (GameObject.FindWithTag("GameOverCamera")).GetComponent(typeof(Camera)) as Camera;
+
+      mainSceneCamera.enabled =  false;
+      menuCamera.enabled =       true;
+      gameOverCamera.enabled =   false;
+      
+      /* Load prefab in the array */
+      GamePrefab.Add(Prototype1);
+      GamePrefab.Add(Prototype2);
+      GamePrefab.Add(Prototype3);
+      GamePrefab.Add(Prototype4);
+      GamePrefab.Add(Prototype5);
+      GamePrefab.Add(Prototype6);
+      GamePrefab.Add(Prototype7);
+      GamePrefab.Add(Prototype8);
+      GamePrefab.Add(Prototype9);
+      GamePrefab.Add(Prototype10);
+
+      /* TODO: Algorithm to select and sort elements of listObjects */
+      /*------------------------------------------------------------*/
+      /**************************************************************/
+      
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+      
+      /* Videogame controller State Machine */
+      switch (MachineState)
+      {
+         case en_MachineState.IDLE:
+            /* Idle state. Once loading of all components is terminated, the machine goes into Opening scene state */
+            
+            MachineState = en_MachineState.OPENING;
+            break;
+            
+         case en_MachineState.OPENING:
+            /* Opening state. Use this to show cinematic scene at the opening. Then go to Main Menu scene */
+            
+            MachineState = en_MachineState.MAINMENU;
+            break;
+            
+         case en_MachineState.MAINMENU:
+            /* Wait for the player to start by pushing the New Game Button and go into the Intro state */ 
+            /* Set the main menu camera active */
+            mainSceneCamera.enabled =  false;
+            menuCamera.enabled =       true;
+            gameOverCamera.enabled =   false;
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+               /* Input mouse position is mapped to the 3D World (only work for orthogonal camera) */
+               /* NOTE: Camera Component should be tagged "MainCamera" to avoid missing pointer    */
+               Vector3 CursorPosition = menuCamera.ScreenToWorldPoint(Input.mousePosition);
+               CursorPosition.z = 0.0F; // 2D subspace, where game happen, is [Z=0]
+
+               /* Transform the click of the mouse in a Ray */
+               Ray ray = menuCamera.ScreenPointToRay(Input.mousePosition);
+               RaycastHit hit;
+
+               /* Detect a collision of the Ray with a collider and that the collider gameObject is 
+                  tagged as StartButton*/
+               if(Physics.Raycast(ray,out hit) && hit.transform.tag == "StartButton")
+               {
+                  /* Instantiate LIST_ELEMENTS_NUM Prefabs */
+                  i = 0;
+                  foreach (GameObject GamePrefab in GamePrefab) 
+                  {
+                     Instantiate<GameObject>(GamePrefab, new Vector3(0,(5.0F-1.55F * i),0), Quaternion.identity);
+                     i++;
+                  }
+                  
+                  MachineState = en_MachineState.INTRO;
+               }
+            }
+
+            break;
+            
+         case en_MachineState.INTRO:
+            /* Play a cinematic introduction to the main scene of the game */
+            
+            MachineState = en_MachineState.SCENE;
+            initialTime = Time.time;
+            break;
+            
+         case en_MachineState.SCENE:
+            /* Main scene of the game. At the end, switch to Cinematic Outro state */
+            mainSceneCamera.enabled =  true;
+            menuCamera.enabled =       false;
+            
+            /* Go to end scene when the time is finished */
+            if (Time.time - initialTime > mainSceneDurationSeconds)
+            {
+               MachineState = en_MachineState.OUTRO;
+            }
+            break;      
+            
+         case en_MachineState.OUTRO:
+            /* Play a cinematic outro if available. Then switch to Endgame state */            
+            mainSceneCamera.enabled =  false;
+            menuCamera.enabled =       false;
+            gameOverCamera.enabled =   true;
+            
+            MachineState = en_MachineState.ENDGAME;
+            break;   
+            
+         case en_MachineState.ENDGAME:
+            /* Display player score and unlocked future challenges. Then switch to Main Menu */
+            
+            // MachineState = en_MachineState.MAINMENU;
+            break;   
+            
+      }
+	}
+}
---
+++ Project001/WinterSale_ProjectSample/Assets/Scenes/ObjectBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehavior : MonoBehaviour {
+
+   /* Object Movement and Interaction */
+   public float TractionFactorFree     = 0.01F;    // Traction factor during free movement (toward list position)
+   public float TractionFactorCursor   = 0.03F;    // Traction factor during mouse hold (toward cursor position)
+   public float DampingFactor          = 0.15F;    // Damping factor to avoid unwanted oscillations  
+   public float ScrollSpeed         = 0.02F;    // Vertical speed of the list
+   public float TreshX              = 2.5F;     // Horizontal dimension of the GameObject PickUp rectangle
+   public float TreshY              = 0.8F;     // Vertical dimension of the GameObject PickUp rectangle
+   
+   /* This Element Value Parameters */
+   public float   FullPrice         = 60.0F;
+   public float   SalePrice         = 40.0F;
+   public int     ID                = 1;
+      
+   public Camera mainSceneCamera;   
+   private Vector3 TargetPosition; // To store initial value of target position and future evolution
+   private Vector3 Momentum = new Vector3();   // To emulate 'inertia' to the gameobject
+   
+   private enum ObjectState {FREE, HOLD} // State machine to manage input from user / free movement of gameobject
+   private ObjectState state = ObjectState.FREE; // Initialized state to 'free'
+
+	// Use this for initialization
+	void Start () 
+   {
+      /* Set initial target position as the spawn point */
+      TargetPosition = transform.position;
+      /* Store Main Camera as Local Variable */
+      mainSceneCamera = (GameObject.FindWithTag("MainCamera")).GetComponent(typeof(Camera)) as Camera;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+   {  
+      /* Update target position */
+		TargetPosition = TargetPosition + new Vector3(0,ScrollSpeed,0);
+      
+      /* Input mouse position is mapped to the 3D World (only work for orthogonal camera) */
+      /* NOTE: Camera Component should be tagged "MainCamera" to avoid missing pointer    */
+      Vector3 CursorPosition = mainSceneCamera.ScreenToWorldPoint(Input.mousePosition);
+      CursorPosition.z = 0.0F; // 2D subspace where game happen is [Z=0]
+      
+      /* If abs(deltaX) is less than TreshX and abs(deltaY) is less than TreshY => HOLD, else FREE */
+      if (Input.GetMouseButtonDown(0))
+      {
+         if (CursorPosition[0]<(transform.position[0]+TreshX) && CursorPosition[0]>(transform.position[0]-TreshX) &&
+             CursorPosition[1]<(transform.position[1]+TreshY) && CursorPosition[1]>(transform.position[1]-TreshY) )
+             {
+                state = ObjectState.HOLD;
+             }
+      }
+      if (Input.GetMouseButtonUp(0))
+      {
+         state = ObjectState.FREE;
+      }
+      
+      /* Update position of the gameObject */
+      switch (state) 
+      {
+         case ObjectState.FREE:
+            /* NOTE: To simulate presence of inertia, the equation of motion is solved: m x''+ c x' = F, where F is
+               a force trusting in the direction of the target position. Differentiating: m dV + c V = k*Error, therefore
+               dividing by m: dV = k1*Error - k2 V and V(k+1) = (1-k2) V(k) + k1 * Error. The overall transition can be described by tuning k1 and k2 parameters. */
+         
+            Momentum = (1 - DampingFactor) * Momentum + TractionFactorFree * (TargetPosition-transform.position);
+            /* GameObject should move toward TargetPosition */
+            transform.position = transform.position + Momentum;
+            break;
+            
+         case ObjectState.HOLD:
+            /* GameObject should follow the user input */
+            Momentum = (1 - DampingFactor) * Momentum + TractionFactorCursor * (CursorPosition-transform.position);
+            transform.position = transform.position + Momentum;
+            break;
+      }
+	}
+   
+   /* OnCollisionEnter is called whenever this object collider encounter another object collider */
+   // Collision events are only sent if one of the colliders also has a non-kinematic rigidbody attached.
+   void OnCollisionStay(Collision otherObj) 
+   {
+      /* Destroy gameobject if it is released on the Cart */
+      if (Input.GetMouseButtonUp(0) && otherObj.gameObject.name == "CartSample") 
+      {         
+         /* Call GameController method to add gameobject to cart list (avoiding ripetitions)*/
+         if (!GameController.objList.Contains(ID))
+         {
+            GameController.objList.Add(ID);
+         }
+
+         /* TODO: Smooth disappear into the cart */
+         
+         /* Destroy this gameObject from scene */
+         Destroy(gameObject);
+      }
+   }
+}
